6b16f5306fd82fb3e9b0ecbcd420a6b12b8e33c9
hengameh <work.ghasemi@gmail.com>
2023-03-04 14:49:33 +0000

    add app module for using dagger hilt

---
+++ app/src/main/java/com/example/myapplication2/di/AppModule.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication2.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
