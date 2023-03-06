23059aced2006fcca426574fc56eb7e3626416d7
hengameh <work.ghasemi@gmail.com>
2023-03-06 14:42:53 +0000

    add NewsListScreen.kt

--- app/src/main/java/com/example/myapplication2/ui/newsList/NewsListScreen.kt
+++ app/src/main/java/com/example/myapplication2/ui/newsList/NewsListScreen.kt
@@ -1,12 +1,26 @@
 package com.example.myapplication2.ui.newsList
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun NewsListScreen(
-    navController: NavController,
-
-){
-
+    navController: NavController
+) {
+    Surface(
+        color = MaterialTheme.colors.background,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
 }
